50881653d0f8e93b0b199bde4f20727c80101453
OSHIN <izkp44@gmail.com>
2023-04-14 22:19:13 +0700

    Create TypeTest.ts

---
+++ TypeTest.ts
@@ -0,0 +1,17 @@
+var myName: string = 'BlueHa'
+console.log(myName)
+console.log(typeof myName)
+
+let money: number = 10
+console.log(money)
+console.log(typeof money)
+
+console.log(null === undefined)
+console.log(null == undefined)
+console.log(null === null)
+
+let studentName: string = 'Happy'
+let age: number = 9
+let score
+let gender: string
+let isStudent = true
